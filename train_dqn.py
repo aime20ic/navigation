04cceb8fbe99c4aef2919038310cdc03de7d7aae
Ivan Caceres <aime20ic@gmail.com>
2021-05-27 18:07:43 -0500

    setup output dir + figure saving & closing fixes

--- train_dqn.py
+++ train_dqn.py
@@ -1,8 +1,11 @@
+import time
 import torch
 import argparse
 import numpy as np
-from collections import deque
 import matplotlib.pyplot as plt
+
+from pathlib import Path
+from collections import deque
 
 from dqn_agent import DQNAgent
 from banana_env import BananaEnv
@@ -14,47 +17,57 @@ def parse_args():
 
     Args:
         None
-    
+
     Returns:
         Argparse NameSpace object containing command line arguments
-    
+
     """
     parser = argparse.ArgumentParser(description='DQN hyperparameters')
-    parser.add_argument('--buffer-size', type=int, default=int(1e5), 
+    parser.add_argument('--buffer-size', type=int, default=int(1e5),
         help='Experience replay buffer size'
     )
-    parser.add_argument('--batch-size', type=int, default=64, 
+    parser.add_argument('--batch-size', type=int, default=64,
         help='Mini-batch size'
     )
-    parser.add_argument('--eps-start', type=float, default=1.0, 
+    parser.add_argument('--eps-start', type=float, default=1.0,
         help='Starting value of epsilon, for epsilon-greedy action selection'
     )
-    parser.add_argument('--eps-end', type=float, default=0.01, 
+    parser.add_argument('--eps-end', type=float, default=0.01,
         help='Minimum value of epsilon'
     )
-    parser.add_argument('--eps-decay', type=float, default=0.995, 
+    parser.add_argument('--eps-decay', type=float, default=0.995,
         help='Multiplicative factor (per episode) for decreasing epsilon'
     )
-    parser.add_argument('--gammma', type=float, default=0.99, 
+    parser.add_argument('--gammma', type=float, default=0.99,
         help='Discount factor'
     )
-    parser.add_argument('--hidden', nargs='*', default=[256,128,64], 
+    parser.add_argument('--hidden', nargs='*', default=[256,128,64],
         help='Model hidden layer sizes'
     )
     parser.add_argument('--lr', type=float, default=5e-4, help='Learn rate')
-    parser.add_argument('--max-t', type=int, default=300, 
+    parser.add_argument('--max-t', type=int, default=300,
         help='Maximum number of timesteps per episode'
     )
-    parser.add_argument('--n-episodes', type=int, default=2000, 
+    parser.add_argument('--n-episodes', type=int, default=2000,
         help='Maximum number of training episodes'
     )
-    parser.add_argument('--seed', type=int, default=0, 
+    parser.add_argument('--output', type=str, default='./output',
+        help='Dir to save models, logs, & other output'
+    )
+    parser.add_argument('--seed', type=int, default=0,
         help='Seed for repeatability'
     )
-    parser.add_argument('--tau', type=float, default=1e-3, 
+    parser.add_argument('--sim', type=str,
+        default='Banana_Windows_x86_64/Banana.exe',
+        help='Path to Unity Banana simulation'
+    )
+    parser.add_argument('--tau', type=float, default=1e-3,
         help='Interpolation weight for soft update of target parameters'
     )
-    parser.add_argument('--update-every', type=int, default=4, 
+    parser.add_argument('--train-id', type=int,
+        help='Training run identifier'
+    )
+    parser.add_argument('--update-every', type=int, default=4,
         help='Update frequency'
     )
     parser.add_argument('--verbose', action='store_true', help='Verbosity')
@@ -63,32 +76,42 @@ def parse_args():
     # Convert hidden layer argument to list of ints
     args.hidden = [int(size) for size in args.hidden]
 
+    # Convert current time to training ID if none specified
+    if args.train_id is None:
+        args.train_id = int(time.time())
+
+    # Convert output directory to path object including training ID
+    args.output = Path(args.output + '/' + str(args.train_id) + '/')
+
     return args
 
-def dqn(env, agent, n_episodes, max_t, eps_start, eps_end, eps_decay, 
-        verbose=False):
+def dqn(env, agent, **kwargs):
     """
     Deep Q-Learning
-    
+
     Args:
         env (BananaEnv): Unity banana simulation environment
         agent (DQNAgent): Deep q-learning agent
-        n_episodes (int): Maximum number of training episodes
-        max_t (int): Maximum number of timesteps per episode
-        eps_start (float): Starting value of epsilon, for epsilon-greedy action 
-                           selection
-        eps_end (float): Minimum value of epsilon
-        eps_decay (float): multiplicative factor (per episode) for decreasing 
-                           epsilon
-        verbose (boolean): Verbosity
 
     Returns:
         scores (type): Description
-    
+
     """
     scores = []                        # list containing scores from each episode
     scores_window = deque(maxlen=100)  # last 100 scores
-    eps = eps_start                    # initialize epsilon
+
+    # Initialize key word argument variables
+    n_episodes = kwargs.get('n_episodes', 2000)
+    max_t = kwargs.get('max_t', 300)
+    eps_start = kwargs.get('eps_start', 1.0)
+    eps_end = kwargs.get('eps_end', 0.01)
+    eps_decay = kwargs.get('eps_decay', 0.995)
+    train_id = kwargs.get('train_id', int(time.time()))
+    output = kwargs.get('output', Path('./output/' + str(train_id) + '/'))
+    verbose = kwargs.get('verbose', False)
+
+    # Initialize epsilon
+    eps = eps_start
 
     # Train for n_episodes
     for i_episode in range(1, n_episodes+1):
@@ -96,8 +119,8 @@ def dqn(env, agent, n_episodes, max_t, eps_start, eps_end, eps_decay,
         # Reset environment
         score = 0
         state = env.reset()
-        
-        # Learn for max_t steps 
+
+        # Learn for max_t steps
         for t in range(max_t):
 
             # Get action from agent using current policy
@@ -106,7 +129,7 @@ def dqn(env, agent, n_episodes, max_t, eps_start, eps_end, eps_decay,
             # Perform action in environment
             state, action, reward, next_state, done = env.step(action)
 
-            # Update agent 
+            # Update agent
             agent.step(state, action, reward, next_state, done)
 
             # Update next state & score
@@ -115,8 +138,8 @@ def dqn(env, agent, n_episodes, max_t, eps_start, eps_end, eps_decay,
 
             # Check terminal condition
             if done:
-                break 
-        
+                break
+
         # Update scores
         scores_window.append(score)       # save most recent score
         scores.append(score)              # save most recent score
@@ -131,7 +154,7 @@ def dqn(env, agent, n_episodes, max_t, eps_start, eps_end, eps_decay,
             print('\rEpisode {}\tAverage Score: {:.2f}'.format(
                 i_episode, np.mean(scores_window)))
 
-        # In order to solve the environment, your agent must get an average 
+        # In order to solve the environment, your agent must get an average
         # score of +13 over 100 consecutive episodes
         if np.mean(scores_window)>=13.0:
             print('\nEnvironment solved in {:d} episodes!'
@@ -139,9 +162,10 @@ def dqn(env, agent, n_episodes, max_t, eps_start, eps_end, eps_decay,
                     i_episode-100, np.mean(scores_window)
                 )
             )
-            torch.save(agent.qnetwork_local.state_dict(), 'checkpoint.pth')
+            torch.save(agent.qnetwork_local.state_dict(), 
+                output / (str(train_id) + '__checkpoint.pth'))
             break
-    
+
     return scores
 
 def main(args):
@@ -153,36 +177,26 @@ def main(args):
 
     Returns:
         None
-    
+
     """
 
     # Create environment
-    env = BananaEnv(train=True, seed=args.seed, verbose=args.verbose)
+    env = BananaEnv(args.sim, train=True, seed=args.seed, verbose=args.verbose)
 
     # Create agent
-    agent = DQNAgent(
-        state_size=env.state_size, 
-        action_size=env.action_size, 
-        **vars(args)
-    )
+    agent = DQNAgent(env.state_size, env.action_size, **vars(args))
+
+    # Create output directory
+    args.output.mkdir(parents=True, exist_ok=True)
 
     # Perform deep q-learning
-    scores = dqn(
-        env=env, 
-        agent=agent,
-        n_episodes=args.n_episodes, 
-        max_t=args.max_t, 
-        eps_start=args.eps_start, 
-        eps_end=args.eps_end, 
-        eps_decay=args.eps_decay,
-        verbose=args.verbose
-    )
+    scores = dqn(env, agent, **vars(args))
 
     # Close environment
     env.close()
 
     # Create avg score
-    avg_score = [np.mean(scores[:i+1] for i in range(len(scores)))]
+    avg_score = [np.mean(scores[:i+1]) for i in range(len(scores))]
 
     # Plot scores
     fig = plt.figure()
@@ -192,10 +206,10 @@ def main(args):
     plt.ylabel('Score')
     plt.xlabel('Episode #')
     plt.legend(loc='upper left')
-    plt.show()
+    plt.show(block=False)
 
     # Save figure
-    plt.savefig('dqn_performance.png'))
+    plt.savefig(args.output / (str(args.train_id) + '__dqn_performance.png'))
     plt.close()
 
     return
@@ -203,7 +217,7 @@ def main(args):
 
 if __name__ == "__main__":
     """
-    Execute script 
+    Execute script
     """
     args = parse_args()
     main(args)
