05f13996e477cae5abb982faba84d2cb07f91241
Ivan Caceres <aime20ic@gmail.com>
2021-05-28 21:14:52 -0500

    argparse formatting + early stop + save best model

--- navigation/train_dqn.py
+++ navigation/train_dqn.py
@@ -24,63 +24,32 @@ def parse_args():
 
     """
     parser = argparse.ArgumentParser(description='DQN hyperparameters')
-    parser.add_argument('--agent-name', type=str, default='dqn-agent',
-        help='DQN agent name'
-    )
-    parser.add_argument('--buffer-size', type=int, default=int(1e5),
-        help='Experience replay buffer size'
-    )
-    parser.add_argument('--batch-size', type=int, default=64,
-        help='Mini-batch size'
-    )
-    parser.add_argument('--env-name', type=str, default='banana-env',
-        help='BananaEnv name'
-    )
-    parser.add_argument('--eps-start', type=float, default=1.0,
-        help='Starting value of epsilon, for epsilon-greedy action selection'
-    )
-    parser.add_argument('--eps-end', type=float, default=0.01,
-        help='Minimum value of epsilon'
-    )
-    parser.add_argument('--eps-decay', type=float, default=0.995,
-        help='Multiplicative factor (per episode) for decreasing epsilon'
-    )
-    parser.add_argument('--gammma', type=float, default=0.99,
-        help='Discount factor'
-    )
-    parser.add_argument('--hidden', nargs='*', default=[256,128,64],
-        help='Model hidden layer sizes'
-    )
+    parser.add_argument('--agent-name', type=str, default='dqn-agent', help='DQN agent name')
+    parser.add_argument('--buffer-size', type=int, default=int(1e5), help='Experience replay buffer size')
+    parser.add_argument('--batch-size', type=int, default=64, help='Mini-batch size')
+    parser.add_argument('--env-name', type=str, default='banana-env', help='BananaEnv name')
+    parser.add_argument('--eps-start', type=float, default=1.0, 
+        help='Starting value of epsilon, for epsilon-greedy action selection')
+    parser.add_argument('--eps-end', type=float, default=0.01, help='Minimum value of epsilon')
+    parser.add_argument('--eps-decay', type=float, default=0.995, 
+        help='Multiplicative factor (per episode) for decreasing epsilon')
+    parser.add_argument('--gammma', type=float, default=0.99, help='Discount factor')
+    parser.add_argument('--goal', type=float, default=13.0, help='Score goal')
+    parser.add_argument('--hidden', nargs='*', default=[256,128,64], help='Model hidden layer sizes')
     parser.add_argument('--load', type=str, help='Path to model to load')
     parser.add_argument('--lr', type=float, default=5e-4, help='Learn rate')
-    parser.add_argument('--max-t', type=int, default=300,
-        help='Maximum number of timesteps per episode'
-    )
-    parser.add_argument('--n-episodes', type=int, default=2000,
-        help='Maximum number of training episodes'
-    )
-    parser.add_argument('--output', type=str, default='./output',
-        help='Directory to save models, logs, & other output'
-    )
-    parser.add_argument('--run-id', type=int,
-        help='Execution run identifier'
-    )
-    parser.add_argument('--seed', type=int, default=0,
-        help='Seed for repeatability'
-    )
-    parser.add_argument('--sim', type=str,
-        default='Banana_Windows_x86_64/Banana.exe',
-        help='Path to Unity Banana simulation'
-    )
-    parser.add_argument('--tau', type=float, default=1e-3,
-        help='Interpolation weight for soft update of target parameters'
-    )
-    parser.add_argument('--test', action='store_true',
-        help='Test mode, no agent training'
-    )
-    parser.add_argument('--update-every', type=int, default=4,
-        help='Update frequency'
-    )
+    parser.add_argument('--max-t', type=int, default=300, help='Maximum number of timesteps per episode')
+    parser.add_argument('--n-episodes', type=int, default=2000, help='Maximum number of training episodes')
+    parser.add_argument('--output', type=str, default='./output', help='Directory to save models, logs, & other output')
+    parser.add_argument('--run-id', type=int, help='Execution run identifier')
+    parser.add_argument('--seed', type=int, default=0, help='Seed for repeatability')
+    parser.add_argument('--sim', type=str, default='Banana_Windows_x86_64/Banana.exe', 
+        help='Path to Unity Banana simulation')
+    parser.add_argument('--tau', type=float, default=1e-3, 
+        help='Interpolation weight for soft update of target parameters')
+    parser.add_argument('--test', action='store_true', help='Test mode, no agent training')
+    parser.add_argument('--update-every', type=int, default=4, help='Update frequency')
+    parser.add_argument('--window', type=int, default=100, help='Window size to use for terminal condition check')
     parser.add_argument('--verbose', action='store_true', help='Verbosity')
     args = parser.parse_args()
 
@@ -97,30 +66,31 @@ def parse_args():
 
     return args
 
-def plot_performance(scores, name, window):
+def plot_performance(scores, name, window_size):
     """
     Plot summary of DQN performance on environment
 
     Args:
         scores (list of float): Score per simulation episode
         name (Path): Name for file
-        window (int): Windowed average size
+        window_size (int): Windowed average size
 
     """
+    window_avg = []
+    window = deque(maxlen=window_size)
 
     # Create avg score
     avg = [np.mean(scores[:i+1]) for i in range(len(scores))]
-    window_avg = [
-        np.mean(scores[i+1-window:i+1]) if i+1-window >= 0 else np.mean(scores[:i+1]) 
-        for i in range(len(scores))
-    ]
+    for i in range(len(scores)):
+        window.append(scores[i])
+        window_avg.append(np.mean(window))
 
     # Plot scores
     fig = plt.figure()
     ax = fig.add_subplot(111)
     plt.scatter(np.arange(len(scores)), scores, color='cyan', label='Scores')
     plt.plot(avg, color='blue', label='Average')
-    plt.plot(window, color='red', label='Windowed Average (n={})'.format(window))
+    plt.plot(window_avg, color='red', label='Windowed Average (n={})'.format(window_size))
     plt.ylabel('Score')
     plt.xlabel('Episode #')
     plt.legend(loc='upper left')
@@ -173,9 +143,10 @@ def eval_agent(agent, env, eval_type, **kwargs):
 
     """
     scores = []                                 # scores from each episode
-    score_goal = 13.0                           # goal to get to
-    window_size = 100                           # size for rolling window
-    eval_options = ['train', 'test']              # evaluation options
+    best_avg_score = -100                       # best averaged window score
+    score_goal = kwargs.get('goal', 13.0)       # goal to get to
+    window_size = kwargs.get('window', 100)     # size for rolling window
+    eval_options = ['train', 'test']            # evaluation options
     scores_window = deque(maxlen=window_size)   # last 100 scores
 
     # Error check
@@ -235,7 +206,7 @@ def eval_agent(agent, env, eval_type, **kwargs):
         # Save most recent scores
         scores_window.append(score)
         scores.append(score)
-
+        
         # Decrease epsilon
         eps = max(eps_end, eps_decay*eps)
 
@@ -244,27 +215,41 @@ def eval_agent(agent, env, eval_type, **kwargs):
         scores_std = np.std(scores_window)
 
         # Print & log episode performance
-        window_summary = '\rEpisode {}\tAverage Score: {:.2f}±{:.2f}'.format(
-            i_episode, scores_mean, scores_std)
+        window_summary = '\rEpisode {}\tAverage Score: {:.2f} ± {:.2f}'.format(i_episode, scores_mean, scores_std)
         print(window_summary, end="")
         if eval_type == 'test': write2path(window_summary, log)
 
-        # Print & log performance of last window_size runs
+        # Check terminal condition every window_size episodes
         if i_episode % window_size == 0:
-            window_summary = '\rEpisode {}\tAverage Score: {:.2f}±{:.2f}'.format(
-                i_episode, scores_mean, scores_std)
+            
+            # Save best performing model (weights)
+            if scores_mean >= best_avg_score:
+                output.mkdir(parents=True, exist_ok=True)
+                torch.save(agent.qnetwork_local.state_dict(), output / (prefix + '__best_model.pth'))
+                best_avg_score = scores_mean
+
+            # Print & log performance of last window_size runs
+            window_summary = '\rEpisode {}\tAverage Score: {:.2f} ± {:.2f}'.format(i_episode, scores_mean, scores_std)
             print(window_summary)
             write2path(window_summary, log)
 
-        # Terminal condition check
+            # Terminal condition check (early stop / overfitting)
+            if scores_mean < best_avg_score:
+                window_summary = ('\rEarly stop at {:d}/{:d} episodes!\rAverage Score: {:.2f} ± {:.2f}'
+                    '\tBest Average Score: {:.2f}').format(i_episode, n_episodes, scores_mean, scores_std, best_avg_score)
+                print(window_summary)
+                write2path(window_summary, log)
+                break
+
+        # Terminal condition check (hit goal)
         if eval_type == 'train' and scores_mean - scores_std >= score_goal:
-            window_summary = '\nEnvironment solved in {:d} episodes!\tAverage Score: {:.2f}±{:.2f}'.format(
-                i_episode-100, scores_mean, scores_std)
+            window_summary = '\nEnvironment solved in {:d}/{:d} episodes!\tAverage Score: {:.2f}±{:.2f}'.format(
+                i_episode, n_episodes, scores_mean, scores_std)
             print(window_summary)
             write2path(window_summary, log)
             break
 
-    # Save model 
+    # Save final model (weights) 
     output.mkdir(parents=True, exist_ok=True)
     torch.save(agent.qnetwork_local.state_dict(), output / (prefix + '__model.pth'))
     
@@ -283,7 +268,9 @@ def eval_agent(agent, env, eval_type, **kwargs):
         'eps_end': eps_end,
         'eps_decay': eps_decay,
         'agent_seed': agent.rng_seed,
-        'env_seed': env.rng_seed
+        'env_seed': env.rng_seed,
+        'best_avg_score': best_avg_score,
+        'scores_mena': scores_mean
     }
     with open(output / (prefix + '__parameters.json'), 'w') as file:
         json.dump(parameters, file, indent=4, sort_keys=True)
